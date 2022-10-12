f0472fec4379e316d28fa165623a980d721bd5c3
yulunmaps <100971599+yulunwu8@users.noreply.github.com>
2022-10-12 15:02:04 -0400

    include differentiating reflectance in multiprocessing

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -19,53 +19,43 @@ two_up =  path.abspath(path.join(__file__ ,"../.."))
 sys.path.append(two_up)
 
 
-
-
-# Actual imports 
 import tmart
 import numpy as np
 from Py6S.Params.atmosprofile import AtmosProfile
 import Py6S
 
-
-
-# Specify wavelength in nm
-band = Py6S.Wavelength(Py6S.PredefinedWavelengths.S2A_MSI_08)
-band = None
-
-
-wl = 470
-
+# Band 2 of Sentinel-2A and its central wavelength
+band = Py6S.Wavelength(Py6S.PredefinedWavelengths.S2A_MSI_02)
+wl = 490 
 
 ### DEM and reflectance ###
 
 # Three same-size numpy arrays are needed
-image_DEM = np.array([[0,0],[0,0]]) # in meters
-image_reflectance = np.array([[0.0314,0.0314],[0.0314,0.0314]]) # unitless     
-image_isWater = np.zeros(image_DEM.shape) # 1 is water, 0 is land
-image_isWater = np.array([[1,1],[1,1]])
+image_DEM = np.full((2, 2), 0) # in meters
+image_reflectance = np.full((2, 2), 0.1) # unitless     
+image_isWater = np.full((2, 2), 1) # 1 is water, 0 is land
 
+# pixel width and length, in meters 
+cell_size = 20_000 
 
 # Synthesize a surface object
 my_surface = tmart.Surface(DEM = image_DEM,
                            reflectance = image_reflectance,
                            isWater = image_isWater,
-                           cell_size = 20_000)  
-
+                           cell_size = cell_size)  
 # Set background information, 1 or 2 background surfaces can be set;
 # If 2: the first background is the one closer to [0,0]
-my_surface.set_background(bg_ref        = 0.0314, # background reflectance
-                          bg_isWater    = 1, # if is water
+my_surface.set_background(bg_ref        = [0.1,0.1], # background reflectance
+                          bg_isWater    = [1,1], # if is water
                           bg_elevation  = 0, # elevation of both background
-                          bg_coords     = [[0,0],[10,10]]) # a line dividing two background                                    
-
+                          bg_coords     = [[0,0],[10,10]]) # a line dividing the two background                                    
 
 ### Atmosphere ###
 
 # Atmophere profile comes from 6S
 atm_profile = AtmosProfile.PredefinedType(AtmosProfile.MidlatitudeSummer) 
 aerosol_type = 'Maritime' 
-aot550 = 0
+aot550 = 0.1
 n_layers = 20
 aerosol_scale_height = 2 # Unless you have a reason, don't change this
 
@@ -74,49 +64,26 @@ my_atm = tmart.Atmosphere(atm_profile, aot550, aerosol_type, n_layers, aerosol_s
 
 ### Running T-Mart ###
 
-# Make a T-Mart object and modify wind and water parameters 
-my_tmart = tmart.Tmart(Surface = my_surface, Atmosphere= my_atm, shadow=False)
-my_tmart.set_wind(wind_speed=10, wind_dir=0)
+# Make a T-Mart object 
+my_tmart = tmart.Tmart(Surface = my_surface, Atmosphere= my_atm, shadow=True)
+my_tmart.set_wind(wind_speed=10, wind_azi_avg = True)
 my_tmart.set_water(water_salinity=35, water_temperature=20)
-
 
 # Specify the sensor's position (x, y, z), viewing direction relative 
 # to the sensor (zenith, azimuth), sun's direction relative to the target 
-# (zenith, azimuth)
+# (zenith, azimuth), see Parameters for more geometry options
 my_tmart.set_geometry(sensor_coords=[51,50,130_000], 
                       target_pt_direction=[180,0],
                       sun_dir=[0,0])
 
-# Run
+# Number of photons
+n_photon = 100_000
 
-
-
-
-'''
-results = my_tmart.run_plot(wl=wl, band=band, plot_on=True, plot_range=[0,100000,0,100000,0,100000])
-
-'''
-
-
-n_photon = 10_000
-results = my_tmart.run(wl=wl, band=band, n_photon=n_photon,nc= 10,njobs= 100)
-results = np.vstack(results)
-
+results = my_tmart.run(wl=wl, band=band, n_photon=n_photon)
 # Calculate reflectances using recorded photon information 
 R = tmart.calc_ref(results, detail=True)
 for k, v in R.items():
     print(k, '     ' , v)
-
-
-
-
-
-
-
-
-
-
-
 
 
 
