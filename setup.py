bc391773aeacf35e25e99e8f5b28cb5514a5eba9
yulunmaps <100971599+yulunwu8@users.noreply.github.com>
2023-04-18 00:14:17 -0400

    1.3.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tmart",                     # This is the name of the package
-    version="1.3.3",                       
+    version="1.3.5",                       
     author="Yulun Wu",                     # Full name of the author
     description="Radiative transfer modelling for aquatic remote sensing",
     long_description=long_description,      # Long description read from the the readme file
