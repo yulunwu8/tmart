f6e2a0edbcb7bd7c2424fee47c4110778e2a60b4
yulunmaps <100971599+yulunwu8@users.noreply.github.com>
2026-01-15 13:42:56 -0500

    Update run_regular.py

--- tmart/AEC/run_regular.py
+++ tmart/AEC/run_regular.py
@@ -41,7 +41,6 @@ def run_regular(file, username, password, AOT, AOT_offset, n_photon, AEC_record,
     
     # compute cloud and non-Water masks 
     print('\nComputing masks: ')
-    
     mask_cloud = tmart.AEC.compute_masks(metadata, config, 'cloud')
     mask_all   = tmart.AEC.compute_masks(metadata, config, 'all')   
     print('Done')
